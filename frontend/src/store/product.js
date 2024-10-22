22f03b8eab8fa30e864fc0a83e208151fb8fc30a
brianrono <brianrono@users.noreply.github.com>
2024-10-22 11:29:03 +0300

    Implement Zustand store for product management

---
+++ frontend/src/store/product.js
@@ -0,0 +1,36 @@
+import { create } from "zustand";
+
+export const useProductStore = create ((set) => ({//for global use
+   products: [],
+   setProducts: (products) => set({products}),
+   createProduct: async (newProduct) => {
+      if(!newProduct.name || !newProduct.price || !newProduct.image) {
+         return {success: false, message: "Please fill in all the fields."};
+      }
+      const res = await fetch("/api/products", {
+         method: "POST",
+         headers: {
+            "Content-Type": "application/json",
+         },
+         body: JSON.stringify(newProduct)
+      });
+
+      if (!res.ok) {
+         let errorMessage = "Failed to create product.";
+         try {
+            const errorData = await res.json(); // Attempt to read the error response this is line 19
+            errorMessage = errorData.message || errorMessage;
+         }
+         catch (error) {
+            console.error("Failed to parse error response:", error);
+         }
+         return { success: false, message: errormessage };
+      }
+
+      const  data = await res.json();
+      set((state) => ({ products:[...state.products, data.data] }));
+      return {success: true, message: "Product created successfully."};
+   }
+}));
+
+// const [state, useState] = useState([]); //used locally 
