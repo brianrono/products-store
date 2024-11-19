68a2165e30bb4bbbc3131c648872404bce92fc7d
brianrono <brianrono@users.noreply.github.com>
2024-11-19 10:16:42 +0300

    Clean code

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import { connectDB } from './config/db.js';
-import productRoutes from './routes/product.route.js';
+import express from "express";
+import dotenv from "dotenv";
+import { connectDB } from "./config/db.js";
+import productRoutes from "./routes/product.route.js";
 
 dotenv.config();
 
@@ -14,5 +14,5 @@ app.use("/api/products", productRoutes);
 
 app.listen(PORT, () => {
     connectDB();
-    console.log('Server started at http://localhost:' + PORT);
+    console.log("Server started at http://localhost:"+ PORT);
 });
