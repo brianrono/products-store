cb900756bc72a2dd85f5985f2a1bdf51fe6fe155
brianrono <brianrono@users.noreply.github.com>
2024-12-02 22:26:57 +0300

    Refactor create page

--- frontend/src/pages/CreatePage.jsx
+++ frontend/src/pages/CreatePage.jsx
@@ -10,69 +10,63 @@ const CreatePage = () => {
    });
 
    const toast = useToast();
-
-   const{createProduct} = useProductStore();
+   const { createProduct } = useProductStore();
 
    const handleAddProduct = async () => {
-      const {success, message} = await createProduct(newProduct);
-      // const productData = {
-      //    ...newProduct,
-      //    price: parseFloat(newProduct.price) || 0, //convert to number or set default value if parsing fails
-      // };
+      const { success, message } = await createProduct(newProduct);
 
-      // const response = await createProduct(productData);
-      // const {success, message} = response;
-      if(!success) {
-         toast({
-            title: "Error creating",
-            description: message,
-            status: "error creating",
-            // duration: 5000
-            isClosable: true
-         });
-      }else {
-         toast({
-            title: "Success",
-            description: message,
-            status: "success",
-            isClosable: true
-         });
+      toast({
+         title: success ? "Success" : "Error",
+         description: message,
+         status: success ? "success" : "error",
+         isClosable: true,
+      });
+
+      if (success) {
+         setNewProduct({ name: "", price: "", image: "" });
       }
-      setNewProduct({name:"", price: "", image: ""});//reset state after product is created
    };
 
    return (
-      <Container maxw={"container.sm"}>
-         <VStack
-            spacing={8}
-         >
-            <Heading as={"h1"} size={"xl"} textAlign={"center"} mb={8}>
-               Create a new product
+      <Container maxW="container.sm">
+         <VStack spacing={8}>
+            <Heading as="h1" size="2xl" textAlign="center" mb={8}>
+               Create New Product
             </Heading>
-            <Box w={"full"} bg={useColorModeValue("white", "gray.800")}
-               padding={6} rounded={"lg"} shadow={"md"}
+            <Box
+               w="full"
+               bg={useColorModeValue("white", "gray.800")}
+               p={6}
+               rounded="lg"
+               shadow="md"
             >
                <VStack spacing={4}>
                   <Input
                      placeholder="Product Name"
                      name="name"
                      value={newProduct.name}
-                     onChange={(e) => setNewProduct({ ...newProduct, name: e.target.value })}
+                     onChange={(e) =>
+                        setNewProduct({ ...newProduct, name: e.target.value })
+                     }
                   />
                   <Input
                      placeholder="Price"
                      name="price"
                      type="number"
                      value={newProduct.price}
-                     onChange={(e) => setNewProduct({ ...newProduct, price: e.target.value })}
+                     onChange={(e) =>
+                        setNewProduct({ ...newProduct, price: e.target.value })
+                     }
                   />
                   <Input
                      placeholder="Image URL"
                      name="image"
                      value={newProduct.image}
-                     onChange={(e) => setNewProduct({ ...newProduct, image: e.target.value })}
+                     onChange={(e) =>
+                        setNewProduct({ ...newProduct, image: e.target.value })
+                     }
                   />
-                  <Button colorScheme="blue" onClick={handleAddProduct} w='full'>
+                  <Button colorScheme="blue" onClick={handleAddProduct} w="full">
                      Add Product
                   </Button>
                </VStack>
@@ -80,5 +74,6 @@ const CreatePage = () => {
          </VStack>
       </Container>
    );
-}
+};
+
 export default CreatePage;
